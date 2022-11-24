746380ec29d6b2d01299f8e440ef8430596d3ec1
Kenzo <lucaskenzok@outlook.com>
2022-11-24 14:37:55 -0300

    Commmit AF

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-from estruturas import *
-from ordem import *
-
+from Estruturas import *
+from Ordem import *
 fila_a = Queue()
 fila_b = Queue()
-C_pilha = Stack()
+pilha_c = Stack()
 
 lista_pessoas = []
 emergencia = []
+valido = False
 
-while True:
+while not valido:
     nome = str(input('Qual seu nome:'))
     print("Prioridade: A = Comum, B = Preferencial ou C para emergencia")
     print('(C1,C2,C3 para cada nivel de emergencia)')
     prioridade = str(input('Qual seu nivel de Priordade: ')).upper()
-    while prioridade not in 'AB,C1,C2,C3':
+
+    if prioridade not in 'AB,C1,C2,C3':
         prioridade = str(input('ERRO!!! digite apenas A,B ou C: ')).upper()
-
-    if prioridade in 'C1C2C3':
-        emergencia.append(prioridade + '--' + nome)
-    elif prioridade in 'A':
-        fila_a.add(prioridade + '--' + nome)
-    elif prioridade in 'B':
-        fila_b.add(prioridade + '--' + nome)
-
+    else:
+        if prioridade in 'C1C2C3':
+            emergencia.append(prioridade + '--' + nome)
+        elif prioridade in 'A':
+            fila_a.add(prioridade + '--' + nome)
+        elif prioridade in 'B':
+            fila_b.add(prioridade + '--' + nome)
+    
     r = str(input('Quer continuar?(N|S): ')).upper()
-    if r in 'N':
-        break
     while r not in 'SN':
         r = str(input('E RRO!!! digite apenas S ou N: ')).upper()
+    if r in 'N':
+        break  
 
-for c in sorted(emergencia, reverse= True):
-    C_pilha.push(c)
-
-Ordem(fila_a,fila_b)
+Ordem.emergencia(emergencia,pilha_c)
+Ordem.Comum_pref(fila_a,fila_b)
--- ordem.py
+++ ordem.py
@@ -1,24 +1,33 @@
 ## ordem
-from estruturas import *
-def Ordem(fila_x,fila_y):
-    tam = fila_y.size() + fila_x.size()
+from Estruturas import *
 
-    for c in range(tam):
-        if fila_y.size() >= 2:
-            print(fila_y.remove())
-            print(fila_y.remove())
-        elif fila_y.size() == 1:
-            print(fila_y.remove())  
+class Ordem:
+    def Comum_pref(fila_x,fila_y):
+        tam = fila_y.size() + fila_x.size()
 
-        if fila_x.isEmpty() == True:
-            return 0
-        else:
-            print(fila_x.remove())
-    
-        if fila_x.isEmpty() == True:
-            for i in range(fila_y.size()):
+        for c in range(tam):
+            if fila_y.size() >= 2:
                 print(fila_y.remove())
-        if fila_y.isEmpty() == True:
-            for i in range(fila_y.size()):
                 print(fila_y.remove())
-    
+            elif fila_y.size() == 1:
+                print(fila_y.remove())  
+
+            if fila_x.isEmpty() == True:
+                return 0
+            else:
+                print(fila_x.remove())
+        
+            if fila_x.isEmpty() == True:
+                for i in range(fila_y.size()):
+                    print(fila_y.remove())
+            if fila_y.isEmpty() == True:
+                for i in range(fila_y.size()):
+                    print(fila_y.remove())
+
+    def emergencia(lista,pilha):
+        for c in sorted(lista):
+            pilha.push(c)
+        while pilha.isEmpty() == False:
+            print(pilha.pop())
+
+
