4a7dd2ffd1fc54fc291851495275ac5695ab1171
Kenzo <lucaskenzok@outlook.com>
2022-11-27 21:18:02 -0300

    push AF1

--- ordem.py
+++ ordem.py
@@ -1,5 +1,6 @@
 ## ordem
 from Estruturas import *
+from time import sleep
 
 class Ordem:
     def Comum_pref(fila_x,fila_y):
@@ -8,26 +9,31 @@ class Ordem:
         for c in range(tam):
             if fila_y.size() >= 2:
                 print(fila_y.remove())
+                sleep(0.5)
                 print(fila_y.remove())
+                sleep(0.5)
             elif fila_y.size() == 1:
                 print(fila_y.remove())  
-
+                sleep(0.5)
             if fila_x.isEmpty() == True:
                 return 0
             else:
                 print(fila_x.remove())
-        
+                sleep(0.5)
             if fila_x.isEmpty() == True:
                 for i in range(fila_y.size()):
                     print(fila_y.remove())
+                    sleep(0.5)
             if fila_y.isEmpty() == True:
                 for i in range(fila_y.size()):
                     print(fila_y.remove())
+                    sleep(0.5)
 
     def emergencia(lista,pilha):
         for c in sorted(lista):
             pilha.push(c)
         while pilha.isEmpty() == False:
             print(pilha.pop())
+            sleep(0.5)
 
 
